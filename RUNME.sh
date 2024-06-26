7dbf9c226fe95f54c0cb475172a183ff5ba1d146
skwangles <tree_house@Hotmail.co.nz>
2024-06-26 15:42:58 +1200

    Add ordered .sh to run

--- RUNME.sh
+++ RUNME.sh
@@ -4,19 +4,19 @@
 # Depends on the Stella VSLAM and CampusVirtualInterfaces already being built
 
 # Build the CampusVirtual control program
-g++ ./runCampusVirtual.cpp -o runCampusVirtual
+g++ ./runCampusVirtual.cc -o runCampusVirtual
 
 # Create Map with G block
-./runCampusVirtual ./ G-block-smaller.mp4 g.db
+./runCampusVirtual false ./ G-block-smaller.mp4 test_out.db
 
-# Add S block
-./runCampusVirtual ./ S-Block-From-G.mp4 g.db g-s.db
+# # Add S block
+# ./runCampusVirtual false ./ S-Block-From-G.mp4 g.db g-s.db
 
-# Add F block & FG
- ./runCampusVirtual ./ FG-F-from-G.mp4 g-s.db g-s-f-fg.db
+# # Add F block & FG
+#  ./runCampusVirtual false ./ FG-F-from-G.mp4 g-s.db g-s-f-fg.db
 
-# Add second F
-./runCampusVirtual ./ F-block.mp4 g-s-f-fg.db g-s-f2-fg.db
+# # Add second F
+# ./runCampusVirtual false ./ F-block.mp4 g-s-f-fg.db g-s-f2-fg.db
 
-# Add E block
-./runCampusVirtual ./ E-block-fast.mp4 g-s-f2-fg.db g-s-f-fg-e.db
+# # Add E block
+# ./runCampusVirtual false ./ E-block-fast.mp4 g-s-f2-fg.db g-s-f-fg-e.db
