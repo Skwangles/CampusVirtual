f451394737748e9bd4126e0c1cff4e11778056fc
skwangles <tree_house@Hotmail.co.nz>
2024-07-02 18:11:54 +1200

    Add alternative interface optimised for CampusVirtual viewer

---
+++ SLAM/CampusVirtualInterface/src/map_builder_campus_virtual.cc
@@ -0,0 +1,169 @@
+#ifdef HAVE_PANGOLIN_VIEWER
+#include "pangolin_viewer/viewer.h"
+#endif
+#ifdef HAVE_IRIDESCENCE_VIEWER
+#include "iridescence_viewer/viewer.h"
+#endif
+#ifdef HAVE_SOCKET_PUBLISHER
+#include "socket_publisher/publisher.h"
+#endif
+
+#include "stella_vslam/system.h"
+#include "stella_vslam/config.h"
+#include "stella_vslam/camera/base.h"
+#include "stella_vslam/util/yaml.h"
+
+#include <iostream>
+#include <chrono>
+#include <fstream>
+#include <numeric>
+
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <spdlog/spdlog.h>
+#include <popl.hpp>
+
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+
+#include "save_filenames_to_db.hpp"
+
+#ifdef USE_STACK_TRACE_LOGGER
+#include <backward.hpp>
+#endif
+
+#ifdef USE_GOOGLE_PERFTOOLS
+#include <gperftools/profiler.h>
+#endif
+
+int send_map_to_socket(const std::shared_ptr<stella_vslam::system>& slam,
+                  const std::shared_ptr<stella_vslam::config>& cfg,
+                  const std::string& map_db_path) {
+
+    // create a viewer object
+    // and pass the frame_publisher and the map_publisher
+    std::shared_ptr<socket_publisher::publisher> publisher;
+    publisher = std::make_shared<socket_publisher::publisher>(
+    stella_vslam::util::yaml_optional_ref(cfg->yaml_node_, "SocketPublisher"),
+    slam,
+    slam->get_frame_publisher(),
+    slam->get_map_publisher());
+
+    publisher->run();
+
+    // shutdown the slam process
+    slam->shutdown();
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+#ifdef USE_STACK_TRACE_LOGGER
+    backward::SignalHandling sh;
+#endif
+
+    // create options
+    popl::OptionParser op("Allowed options");
+    auto help = op.add<popl::Switch>("h", "help", "produce help message");
+    auto vocab_file_path = op.add<popl::Value<std::string>>("v", "vocab", "vocabulary file path");
+   
+    auto config_file_path = op.add<popl::Value<std::string>>("c", "config", "config file path");
+
+    auto map_db_path_in = op.add<popl::Value<std::string>>("i", "map-db-in", "load a map from this path", "");
+    auto map_db_path_out = op.add<popl::Value<std::string>>("o", "map-db-out", "store a map database at this path after slam", "");
+
+   
+    try {
+        op.parse(argc, argv);
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << std::endl;
+        std::cerr << op << std::endl;
+        return EXIT_FAILURE;
+    }
+
+     // check validness of options
+    if (help->is_set()) {
+        std::cerr << op << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (!op.unknown_options().empty()) {
+        for (const auto& unknown_option : op.unknown_options()) {
+            std::cerr << "unknown_options: " << unknown_option << std::endl;
+        }
+        std::cerr << op << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (!vocab_file_path->is_set() || !config_file_path->is_set()) {
+        std::cerr << "invalid arguments" << std::endl;
+        std::cerr << std::endl;
+        std::cerr << op << std::endl;
+        return EXIT_FAILURE;
+    }
+
+
+    // setup logger
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] %^[%L] %v%$");
+    spdlog::set_level(spdlog::level::from_str(log_level->value()));
+
+    // load configuration
+    std::shared_ptr<stella_vslam::config> cfg;
+    try {
+        cfg = std::make_shared<stella_vslam::config>(config_file_path->value());
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+#ifdef USE_GOOGLE_PERFTOOLS
+    ProfilerStart("slam.prof");
+#endif
+
+    std::string map_db_in = map_db_path_in->value();
+
+    // build a slam system
+    auto slam = std::make_shared<stella_vslam::system>(cfg, vocab_file_path->value());
+    bool need_initialize = true;
+    // Load Map Database
+    if (!map_db_in.empty()) {
+        need_initialize = false;
+        const auto path = fs::path(map_db_in);
+        if (path.extension() == ".yaml") {
+            YAML::Node node = YAML::LoadFile(path);
+            for (const auto& map_path : node["maps"].as<std::vector<std::string>>()) {
+                if (!slam->load_map_database(path.parent_path() / map_path)) {
+                    return EXIT_FAILURE;
+                }
+            }
+        }
+        else {
+            if (!slam->load_map_database(path)) {
+                return EXIT_FAILURE;
+            }
+        }
+    }
+    slam->startup(need_initialize);
+    slam->disable_mapping_module();
+
+    // run tracking
+    double finish_timestamp = 0.0;
+    if (slam->get_camera()->setup_type_ == stella_vslam::camera::setup_type_t::Monocular) {
+        finish_timestamp = send_map_to_socket(slam,
+                            cfg,
+                            map_db_path_out->value(),);
+    }
+    else {
+        throw std::runtime_error("Invalid setup type: " + slam->get_camera()->get_setup_type_string());
+    }
+    std::cout << "Finish Timestamp" << finish_timestamp << std::endl;
+
+#ifdef USE_GOOGLE_PERFTOOLS
+    ProfilerStop();
+#endif
+
+    return 0;
+}
