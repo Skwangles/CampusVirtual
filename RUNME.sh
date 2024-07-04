337e33488a1738eca3d9640a7bb06abc713c1819
skwangles <tree_house@Hotmail.co.nz>
2024-07-05 11:02:18 +1200

    RUNME

--- RUNME.sh
+++ RUNME.sh
@@ -16,9 +16,10 @@ g++ ./runCampusVirtual.cc -o runCampusVirtual -lboost_program_options
 
 # Create Map with G block
 # ./runCampusVirtual true ./ G-block-2.mp4,G-block-smaller.mp4 --out g.db --media_dir /media/skwangles/KINGSTON/MEDIA/
+# ./runCampusVirtual false ./ G-block-smaller.mp4 --out g-1.db --media_dir /media/skwangles/KINGSTON/MEDIA/
 
 # # View it
-# # ./runCampusVirtual true ./ .  --in g.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --convertToGraph
+./runCampusVirtual true ./ .  --in g-1.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --convertToGraph
 
 
 # # Add S block
@@ -30,4 +31,4 @@ g++ ./runCampusVirtual.cc -o runCampusVirtual -lboost_program_options
 # # # Add E block
 # ./runCampusVirtual true ./ E-block-fast.mp4 --in g-s-f-fg.db --out g-s-f-fg-e.db --media_dir /media/skwangles/KINGSTON/MEDIA/
 
-./runCampusVirtual true ./ . --in g-s-f-fg-e.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --convertToGraph
+# ./runCampusVirtual true ./ . --in g-s-f-fg-e.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --convertToGraph
