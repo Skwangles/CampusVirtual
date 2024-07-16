5ee4581b20eb2a3f5b3ca68c3be1354ceca41d38
Skwangles <tree_house@hotmail.co.nz>
2024-07-17 10:29:57 +1200

    Setup Postgres

--- SETUP_PG.sh
+++ SETUP_PG.sh
@@ -1,11 +1,6 @@
 cd Postgres
 
-wget http://pqxx.org/download/software/libpqxx/libpqxx-4.0.tar.gz
-tar xvfz libpqxx-4.0.tar.gz
-cd libpqxx-4.0
-./configure
-make
-make install
+sudo apt install libpqxx-dev
 
 docker-compose up -d
 
