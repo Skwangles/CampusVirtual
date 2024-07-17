584da0f4d32e0030ee4f32b710f69eb78e4fb720
skwangles <tree_house@Hotmail.co.nz>
2024-07-17 15:43:17 +1200

    Add Postgres calls

--- campusvirtual-backend/src/main.ts
+++ campusvirtual-backend/src/main.ts
@@ -24,41 +24,24 @@ const send_test_image = true
 
 const defaultPose = [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]
 
-app.get('/point/:id/neighbours/:depth', function (req, res) {
+app.get('/point/:id/neighbours/:depth', async function (req: { params: { id: any; depth: any } }, res: { json: (arg0: {}) => void }) {
   const main_point_id = req.params.id
   const neighbours_depth = req.params.depth
+  // Fetch from Edges for keyframe_id0 = id and select keyframe_id1 - JOIN keyframe_id1 ON 
 
-  res.json({
-    neighbours: [
-      {
-        id: 1,
-        pose: defaultPose,
-        timestamp: 0,
-        x: 0,
-        y: 10,
-        z: 10,
-      },
-      {
-        id: 2,
-        pose: defaultPose,
-        timestamp: 0,
-        x: 0,
-        y: 0,
-        z: 14,
-      },
-      {
-        id: 3,
-        pose: defaultPose,
-        timestamp: 0,
-        x: 0,
-        y: 10,
-        z: 20,
-      },
-    ],
-  })
+  const rows = await db.query(`
+    SELECT k.id, k.ts, n.pose, e.is_direct
+    FROM edges e
+    JOIN nodes n ON e.keyframe_id1 = n.keyframe_id
+    JOIN keyframes k ON e.keyframe_id1 = k.id
+    WHERE e.keyframe_id0 = $1;
+    `, [main_point_id]);
+  
+  console.log(rows)
+  res.json(rows)
 })
 
-app.get('/point/:id', async function (req, res) {
+app.get('/point/:id', async function (req: { params: { id: any } }, res: { json: (arg0: any) => void }) {
   const info = await keyframes(db).findOne({ id: req.params.id })
 
   const video = await video_timestamps(db).findOne({
@@ -66,13 +49,19 @@ app.get('/point/:id', async function (req, res) {
     end_ts: { $gte: info.ts },
   })
 
-  res.json({
-    ...info,
-    video_name: video.name,
-  })
+  const rows = await db.query(`
+    SELECT k.id, k.ts, n.pose, convert_from(vt.name, 'UTF-8')
+    FROM nodes n
+    JOIN keyframes k ON n.keyframe_id = k.id
+    JOIN video_timestamps vt ON k.ts >= vt.start_ts AND k.ts <= vt.end_ts
+    WHERE n.keyframe_id = $1;
+    `, [req.params.id]);
+
+  console.log(rows)
+  res.json(rows)
 })
 
-app.get('/image/:id', function (req, res) {
+app.get('/image/:id', function (req: { params: { id: any } }, res: { sendFile: (arg0: string) => void }) {
   const id = req.params.id
 
   res.sendFile('../pictures/test.jpg')
