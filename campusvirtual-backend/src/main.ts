f9ba4c4823d2eb9d9dc1633acefa2ab869fe7cd4
skwangles <tree_house@Hotmail.co.nz>
2024-07-18 13:29:16 +1200

    Depth SELECT query

--- campusvirtual-backend/src/main.ts
+++ campusvirtual-backend/src/main.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import cors from 'cors'
 import express from 'express'
+import fs from 'fs'
 
 import db from './db'
 
@@ -18,7 +19,39 @@ app.get('/', function (req, res) {
 
 const send_test_image = true
 
-const defaultPose = [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1]
+app.get('/point/:id/neighbours/:depth', async function (req, res) {
+  const main_point_id = req.params.id
+  const neighbours_depth = req.params.depth
+
+  const rows = await db.query(`
+    WITH RECURSIVE neighbours AS (
+      SELECT 
+        e.keyframe_id0 AS start_id,
+        e.keyframe_id1 AS keyframe_id,
+        n.ts,
+        n.pose,
+        1 AS depth
+      FROM edges e
+      JOIN nodes n ON e.keyframe_id1 = n.keyframe_id
+      WHERE e.keyframe_id0 = $1
+      UNION ALL
+      SELECT 
+        n1.start_id,
+        e.keyframe_id1,
+        n2.ts,
+        n2.pose,
+        n1.depth + 1
+      FROM neighbours n1
+      JOIN edges e ON e.keyframe_id0 = n1.keyframe_id
+      JOIN nodes n2 ON e.keyframe_id1 = n2.keyframe_id
+      WHERE n1.depth < $2
+    )
+    SELECT DISTINCT keyframe_id, ts, pose
+    FROM neighbours WHERE keyframe_id <> $1;
+    `, [main_point_id, neighbours_depth]);
+
+  res.json(rows.rows)
+})
 
 app.get('/point/:id/neighbours', async function (req: { params: { id: any; depth: any } }, res: { json: (arg0: {}) => void }) {
   const main_point_id = req.params.id
@@ -31,8 +64,7 @@ app.get('/point/:id/neighbours', async function (req: { params: { id: any; depth
     JOIN nodes n ON e.keyframe_id1 = n.keyframe_id
     WHERE e.keyframe_id0 = $1;
     `, [main_point_id]);
-  
-  console.log(rows.rows)
+
   res.json(rows.rows)
 })
 
@@ -45,17 +77,36 @@ app.get('/point/:id', async function (req: { params: { id: any } }, res: { json:
 
     //, convert_from(vt.name, 'UTF-8')
 // JOIN video_timestamps vt ON n.ts >= vt.start_ts AND n.ts <= vt.end_ts
-  console.log(rows.rows)
   res.json(rows.rows[0])
 })
 
 app.get('/image/:ts', function (req: { params: { ts: any } }, res: { sendFile: (arg0: string) => void }) {
   const ts = req.params.ts
 
+
+
+  if (send_test_image){
+    res.sendFile(picturesDir + "test.jpg");
+    console.log("Sending Test")
+    return;
+  }
+
+
   let [date, ms] = String(ts).split(".")
 
-  res.sendFile(picturesDir + date + "." + ms.slice(0,5) + ".png")
-  // res.sendFile(picturesDir + "test.jpg");
+  // Handle quirk with timestamp saving where last number rounds up 
+  const main_path = picturesDir + date + "." + ms.slice(0,5) + ".png"
+  const rounded_up_path = picturesDir + date + "." + ms.slice(0,4) + (Number(ms[4]) + 1) + ".png"
+
+  if (fs.existsSync(main_path)){
+    res.sendFile(main_path)
+  }
+  else if (fs.existsSync(rounded_up_path)){
+  res.sendFile(rounded_up_path)  
+  }
+  else {
+    res.sendFile(picturesDir + "test.jpg")
+  }
 })
 
 const port = 3001
