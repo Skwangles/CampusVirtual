c582d44f369b894f762982661ba856de8a3bcfb4
skwangles <tree_house@Hotmail.co.nz>
2024-07-22 16:52:23 +1200

    Update BUILD scripts

--- BUILD_CAMPUS_VIRTUAL.sh
+++ BUILD_CAMPUS_VIRTUAL.sh
@@ -2,11 +2,9 @@
 echo "MUST BE RUN IN root CampusVirtual/"
 
 # Install custom socket_viewer which acts like Google Street View - forked from https://github.com/stella-cv/socket_viewer.git
-cd CampusVirtual-SocketViewer && sudo npm install && cd ..
+# cd PgSocketViewer && sudo npm install && cd ..
 # Make sure to run this in a separate terminal before running the CampusVirtualInterface with `node app.js`
 
-# Installs MediaSDKTest tool for stitching videos
-sudo dpkg -i CampusVirtual-FileProcessing/libMediaSDK-dev_2.0-3_amd64_ubuntu18.04.deb
 
 # Build ProcessFiles
 cd CampusVirtual-FileProcessing/ && g++ ./ProcessFiles.cpp -o ./ProcessFiles && cd ..
@@ -15,7 +13,9 @@ mkdir -p SLAM/CampusVirtualInterface/build
 cd SLAM/CampusVirtualInterface/build && cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_STACK_TRACE_LOGGER=ON .. && make -j4 && cd ../../../
 
 # Build controller program
-g++ ./runCampusVirtual.cc -o runCampusVirtual -lboost_program_options
+g++ ./ENTRYPOINT/runCampusVirtual.cc -o runCampusVirtual -lboost_program_options
+
+g++ ./ENTRYPOINT/pair_json_to_timestamp.cc -o runProcessJsonFiles -lpqxx -lsqlite3 -lboost_program_options
 
 
 echo "Run 'node app.js' in CampusVirtual_socket_viewer to start the socket viewer - before running the CampusVirtualInterface. Make sure the SLAM/equirectangular.yaml file has the right SocketViewer IP address."
