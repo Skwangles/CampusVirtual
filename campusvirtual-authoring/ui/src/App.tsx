a9e7b4b78085c754ab635a26930e89e0ae63031f
skwangles <tree_house@Hotmail.co.nz>
2024-08-13 10:34:22 +1200

    Creating a starter authoring interface

---
+++ campusvirtual-authoring/ui/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+// import CampusVirtualViewer from './CampusVirtualViewer'
+import VirtualTour from './VirtualTour'
+
+function App() {
+  return (
+    <>
+        <VirtualTour />
+    </>
+  )
+}
+
+export default App
