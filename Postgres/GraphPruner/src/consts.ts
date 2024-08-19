6e733ba083e747b0bb2b291e3d9b843e534ef100
skwangles <tree_house@Hotmail.co.nz>
2024-08-20 09:42:17 +1200

    GraphPruner: Don't merge yDist > Threshold

--- Postgres/GraphPruner/src/consts.ts
+++ Postgres/GraphPruner/src/consts.ts
@@ -1,4 +1,5 @@
 export const COORDS_TO_METRES = 10;
-export const TARGET_CLOSENESS = 8;
-export const ALWAYS_MERGE_CLOSENESS = 3;
-export const RANGE_OF_NEIGHBOUR_INTERSECTION = 15;
+export const TARGET_CLOSENESS = 5;
+export const ALWAYS_MERGE_CLOSENESS = 2;
+export const RANGE_OF_NEIGHBOUR_INTERSECTION = 8;
+export const Y_DIST_THRESHOLD = 4
