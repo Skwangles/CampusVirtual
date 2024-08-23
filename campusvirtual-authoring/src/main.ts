26d248e52b2147d7dd9c4c8627b4a53956ac9673
skwangles <tree_house@Hotmail.co.nz>
2024-08-23 16:11:58 +1200

    Set authoring to empty

--- campusvirtual-authoring/src/main.ts
+++ campusvirtual-authoring/src/main.ts
@@ -11,10 +11,12 @@ const picturesDir = '/home/skwangles/Documents/Honours/CampusVirtual/pictures/'
 const app = express()
 
 app.use(cors({ origin: '*' }))
+//@ts-ignore - We are compiling to CommonJS which has __dirname
 app.use(express.static(path.join(__dirname, '../ui/dist')))
 
 // render browser
-app.get('/', function (req, res) {
+app.get('/', function (req: any, res: { sendFile: (arg0: any) => void }) {
+  //@ts-ignore - Compiling to CommonJS, which has __dirname
   res.sendFile(path.join(__dirname, '../ui/dist', 'index.html'))
 })
 
@@ -22,7 +24,7 @@ const send_test_image = false
 
 const COORDS_TO_METRES = 40;
 
-app.get('/point/:id/neighbours/:distance_thresh_m/:y_dist_thresh_m', async function (req, res) {
+app.get('/point/:id/neighbours/:distance_thresh_m/:y_dist_thresh_m', async function (req: { params: { id: any; distance_thresh_m: any; y_dist_thresh_m: any } }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): { (): any; new(): any; end: { (): void; new(): any } }; new(): any } }; json: (arg0: any[]) => void }) {
   // Used BFS to find all points down the graph within a range
   const minDepth = 3; // case for when point distances are too large to give decent # of options
   const maxDepth = 10;
@@ -58,7 +60,8 @@ app.get('/point/:id/neighbours/:distance_thresh_m/:y_dist_thresh_m', async funct
   // Initialize a queue for BFS and a set to keep track of visited nodes
   const queue: { keyframe_id: number, depth: number }[] = [{ keyframe_id: mainPointId, depth: 0 }];
   const visited = new Set<number>();
-  const result = new Set(); // keyframe_id => list of neighbors
+  const result = new Set<number>(); // keyframe_id => list of neighbors
+  const output = [];
 
   visited.add(mainPointId);
 
@@ -81,28 +84,28 @@ app.get('/point/:id/neighbours/:distance_thresh_m/:y_dist_thresh_m', async funct
     );
 
     for (const neighbour of neighbours) {
-      const { keyframe_id: neighborId, x_trans: x2, y_trans: y2, z_trans: z2 } = neighbour;
+      const { keyframe_id: neighbourId, x_trans: x2, y_trans: y2, z_trans: z2 } = neighbour;
 
       // Check distance
       const distance = getDistance(x1 * COORDS_TO_METRES, y1 * COORDS_TO_METRES, z1 * COORDS_TO_METRES, x2 * COORDS_TO_METRES, y2 * COORDS_TO_METRES, z2 * COORDS_TO_METRES);
       if (distance < distanceThreshold || depth < minDepth) {
-        if (Number(mainPointId) !== Number(neighborId) && !result.has(neighbour)) {
-          result.add(neighbour);
+        if (Number(mainPointId) !== Number(neighbourId) && !result.has(neighbourId)) {
+          result.add(neighbourId);
+          output.push(neighbourId);
         }
 
-        if (!visited.has(neighborId)) {
-          visited.add(neighborId);
-          queue.push({ keyframe_id: neighborId, depth: depth + 1 });
+        if (!visited.has(neighbourId)) {
+          visited.add(neighbourId);
+          queue.push({ keyframe_id: neighbourId, depth: depth + 1 });
         }
       }
     }
   }
 
-  console.log([...result.values()].map((val: any) => val.keyframe_id))
-  res.json([...result.values()])
+  res.json(output)
 })
 
-app.get('/floorplan/:id', async function (req: any, res: any) {
+app.get('/floorplan/:id', async function (req: { params: { id: any } }, res: any) {
   const id = req.params.id;
   const rows = await db.query(`
 			SELECT n.keyframe_id, n.ts, n.pose
@@ -110,6 +113,7 @@ app.get('/floorplan/:id', async function (req: any, res: any) {
 			JOIN node_locations l ON l.name = n.location
 			WHERE l.location = 
 				(SELECT location FROM nodes WHERE keyframe_id = $1)`, [id])
+  // select from NODES, because it is the superset of refined_nodes, so regardless if using 'dense', or 'refined' viewing you'll always get a hit
   res.json(rows.rows)
 })
 
@@ -125,7 +129,7 @@ app.get('/point/:id', async function (req: { params: { id: any } }, res: { json:
 })
 
 const extension = ".png"
-app.get('/image/:detail/:ts', function (req: { params: { ts: string, detail: string } }, res) {
+app.get('/image/:detail/:ts', function (req: { params: { ts: string, detail: string } }, res: any) {
   const ts = Number(req.params.ts).toFixed(5);
   if (ts === "NaN") {
     res.status(400).send("Image id was not a valid image ID")
