1aa5851825832f3088dfd571c38e2eb3b9b09248
skwangles <tree_house@Hotmail.co.nz>
2024-08-23 16:11:58 +1200

    Set authoring to empty

--- campusvirtual-authoring/ui/src/App.tsx
+++ campusvirtual-authoring/ui/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 // import CampusVirtualViewer from './CampusVirtualViewer'
-import VirtualTour from './VirtualTour'
 
 function App() {
   return (
     <>
-        <VirtualTour />
-    </>
+          </>
   )
 }
 
