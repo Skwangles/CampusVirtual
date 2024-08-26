d8168bf591a8782e33697d817239b692a6063938
skwangles <tree_house@Hotmail.co.nz>
2024-08-26 18:41:07 +1200

    Authoring back: add updating, and init points in general shape

--- authoring/src/server.ts
+++ authoring/src/server.ts
@@ -38,20 +38,19 @@ app.post('/api/floorplans/:name/update', (req: Request, res: Response) => {
   const { name } = req.params;
   const { id, x, y } = req.body;
 
-  if (floorplans[name]) {
-    const node = floorplans[name].nodes.find((node: any) => node.id === id);
-    if (node) {
-      node.x = x;
-      node.y = y;
-      res.sendStatus(200);
-      console.log(floorplans[name])
-    } else {
-      res.status(404).send('Node not found');
+  db.query("UPDATE floorplan_points SET x = $1, y = $2 WHERE keyframe_id = $3 AND location = $4;", [x, y, id, name]).then(
+    (val: any) => {
+      console.log(val)
+      if (val.rowCount > 0) {
+        res.sendStatus(200)
+      }
+      else {
+        res.status(404).send("Could not find point to update")
+      }
     }
-  } else {
-    res.status(404).send('Floorplan not found');
-  }
-
+  ).catch(() => {
+    res.status(404).send("Error updating point")
+  });
 
 });
 
@@ -59,20 +58,20 @@ app.get('/api/floorplans/:name', async (req: Request, res: Response) => {
   const { name } = req.params;
 
   // Includes keyframes bordering the floor (add 'AND type < 50' to exclude)
-  const point_result = await db.query("SELECT keyframe_id, x, y, type FROM floorplan_points WHERE location = $1", [name]);
-  const image = await db.query("SELECT path FROM floorplan_images WHERE location = $1 LIMIT 1;")
+  const pointResult = await db.query("SELECT keyframe_id, x, y, type FROM floorplan_points WHERE location = $1", [name]);
+  const image = await db.query("SELECT path FROM floorplan_images WHERE location = $1 LIMIT 1;", [name])
 
-  const edges = await db.query(`SELECT  e.keyframe_id0, e.keyframe_id1 FROM refined_edges e JOIN floorplan_points f ON e.keyframe_id = f.keyframe_id WHERE f.location = $1`, [name])
-  if (point_result.rowCount && point_result.rowCount > 0 && image.rowCount && image.rowCount > 0) {
-    res.json({ nodes: point_result.rows, image: image.rows[0].path, edges })
+  const edges = await db.query(`SELECT  e.keyframe_id0, e.keyframe_id1 FROM refined_edges e JOIN floorplan_points f ON e.keyframe_id0 = f.keyframe_id WHERE f.location = $1 AND f.type < $2`, [name, BORDERING_FLOOR_POINT_DEFAULT_TYPE])
+  if (pointResult.rowCount && pointResult.rowCount > 0 && image.rowCount && image.rowCount > 0) {
+    res.json({ nodes: pointResult.rows, image: image.rows[0].path, edges: edges.rows })
   } else {
     res.status(404).send('Floorplan not found');
   }
 });
 
 app.get('/api/floorplans', async (req: Request, res: Response) => {
-  const floorplan_results = await db.query("SELECT DISTINCT location FROM floorplan_images;")
-  res.json(floorplan_results.rows.map((val: { location: string }) => val.location));
+  const floorplanResults = await db.query("SELECT DISTINCT location FROM floorplan_images;")
+  res.json(floorplanResults.rows.map((val: { location: string }) => val.location));
 });
 
 async function initDB() {
@@ -101,28 +100,46 @@ async function initDB() {
   try {
     await db.query("BEGIN;");
     // Do by location to avoid causing a possible crash/memory overload
-    const locations_result = await db.query(`SELECT DISTINCT location FROM refined_node_locations;`)
-    const locations = locations_result.rows.map((location: { location: string }) => location.location);
+    const locationsResult = await db.query(`SELECT DISTINCT location FROM refined_node_locations;`)
+    const locations = locationsResult.rows.map((location: { location: string }) => location.location);
     console.log("Locations:", locations)
     for (const location of locations) {
-      const nodes_result = await db.query(`SELECT n.keyframe_id, l.location FROM refined_nodes n JOIN refined_node_locations l ON l.keyframe_id = n.keyframe_id WHERE l.location = $1;`, [location])
-      const border_nodes_result = await db.query(`SELECT keyframe_id1 as keyframe_id, l2.location FROM refined_edges e 
+      const nodesResult = await db.query(`SELECT n.keyframe_id, l.location, n.x_trans, n.z_trans FROM refined_nodes n JOIN refined_node_locations l ON l.keyframe_id = n.keyframe_id WHERE l.location = $1;`, [location])
+      const borderNodesResult = await db.query(`SELECT keyframe_id1 as keyframe_id, l2.location, n.x_trans, n.z_trans FROM refined_edges e 
                     JOIN refined_node_locations l 
                     ON e.keyframe_id0 = l.keyframe_id 
                     JOIN refined_node_locations l2 ON e.keyframe_id1 = l2.keyframe_id
+                    JOIN refined_nodes n ON n.keyframe_id = e.keyframe_id1
                     WHERE l.location = $1 AND l2.location <> $1;
                     `, [location])
 
-      const nodes = nodes_result.rows
-      const border_nodes = border_nodes_result.rows;
-      console.info(`Location ${location} - Nodes: ${nodes_result.rowCount}, Border Nodes: ${border_nodes_result.rowCount}`)
+      const nodes = nodesResult.rows
+      const borderNodes = borderNodesResult.rows;
+      console.info(`Location ${location} - Nodes: ${nodesResult.rowCount}, Border Nodes: ${borderNodesResult.rowCount}`)
+
+      const combined_nodes = [...nodes, ...borderNodes]
+
+      const maxX = Math.max(...combined_nodes.map((node: { x_trans: number }) => node.x_trans))
+      const maxZ = Math.max(...combined_nodes.map((node: { z_trans: number }) => node.z_trans))
+      const minX = Math.min(...combined_nodes.map((node: { x_trans: number }) => node.x_trans))
+      const minZ = Math.min(...combined_nodes.map((node: { z_trans: number }) => node.z_trans))
+
+      const clampToPostive = (val: number) => {
+        return Math.max(0, Math.min(1, val))
+      }
 
       for (const node of nodes) {
         // Don't need to wait for this
-        db.query("INSERT INTO floorplan_points (location, keyframe_id, type) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING;", [location, node.keyframe_id, FLOOR_POINT_DEFAULT_TYPE])
+        const initX = clampToPostive((node.x_trans - minX) / (maxX - minX))
+        const initY = clampToPostive((node.z_trans - minZ) / (maxZ - minZ))
+        db.query("INSERT INTO floorplan_points (location, keyframe_id, type, x, y) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;", [location, node.keyframe_id, FLOOR_POINT_DEFAULT_TYPE, initX, initY])
       }
-      for (const border_node of border_nodes) {
-        db.query("INSERT INTO floorplan_points (location, keyframe_id, type) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING;", [location, border_node.keyframe_id, BORDERING_FLOOR_POINT_DEFAULT_TYPE])
+
+      for (const border_node of borderNodes) {
+        const initX = clampToPostive((border_node.x_trans - minX) / (maxX - minX))
+        const initY = clampToPostive((border_node.z_trans - minZ) / (maxZ - minZ))
+
+        db.query("INSERT INTO floorplan_points (location, keyframe_id, type, x, y) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;", [location, border_node.keyframe_id, BORDERING_FLOOR_POINT_DEFAULT_TYPE, initX, initY])
       }
 
       db.query(`INSERT INTO floorplan_images (location) VALUES ($1) ON CONFLICT DO NOTHING`, [location]);
