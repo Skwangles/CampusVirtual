cbb419425bfaac61f77a86bb1205a823f7b19488
skwangles <tree_house@Hotmail.co.nz>
2024-08-28 11:01:25 +1200

    Add PROJECTNAME to consts

--- campusvirtual-backend/ui/src/consts.ts
+++ campusvirtual-backend/ui/src/consts.ts
@@ -5,3 +5,4 @@ export const showMap = true;
 export const showList = true;
 export const addQuotationMarks = false;
 export const MAX_MAP_HEIGHT_PERCENT = 0.7
+export const PROJECT_NAME = "CampusVirtual"
