441ab6627f063e2594c72bd28bc347c1a99a461f
skwangles <tree_house@Hotmail.co.nz>
2024-08-27 15:59:13 +1200

    Change FloorPlan Image dir to absolute

--- campusvirtual-backend/src/consts.ts
+++ campusvirtual-backend/src/consts.ts
@@ -3,7 +3,7 @@ export const FLOOR_POINT_DEFAULT_TYPE = 0;
 export const KEYFRAME_IMG_DIR = '/home/skwangles/Documents/Honours/CampusVirtual/pictures/'
 export const SEND_TEST_IMAGE = false
 export const COORDS_TO_METRES = 10;
-export const FLOORPLAN_IMAGE_DIR = "../ui/uploads"
+export const FLOORPLAN_IMAGE_DIR = "/home/skwangles/Documents/Honours/CampusVirtual/floorplans/"
 export const KEYFRAME_IMG_EXTENSION = ".png"
 
 export const DISABLE_AUTHORING_PAGE = false;
