9bc012769c149a3b0816b13de1e44b86e564dd9a
skwangles <tree_house@Hotmail.co.nz>
2024-08-28 12:32:39 +1200

    graphpruner: move utils to utils file

--- Postgres/GraphPruner/src/consts.ts
+++ Postgres/GraphPruner/src/consts.ts
@@ -2,3 +2,5 @@ export const COORDS_TO_METRES = 10;
 export const TARGET_CLOSENESS = 3;
 export const ALWAYS_MERGE_CLOSENESS = 0.5;
 export const Y_DIST_THRESHOLD = 4
+export const KEYFRAME_IMG_EXTENSION = ".png"
+export const KEYFRAME_IMG_DIR = "/home/skwangles/Documents/Honours/CampusVirtual/pictures/"
