62be3e9e235445e33e25a32e7a27debf7f7469ac
skwangles <tree_house@Hotmail.co.nz>
2024-08-28 13:00:20 +1200

    Authoring: Now looks up map image with API endpoint

--- authoring/src/FloorplanList.tsx
+++ authoring/src/FloorplanList.tsx
@@ -8,7 +8,7 @@ interface FloorplanListProps {
 const FloorplanList: React.FC<FloorplanListProps> = ({ floorplans, onSelect }) => {
   floorplans = floorplans.sort()
   return (
-    <div style={{border: "solid 2px", height: "100vh", marginRight: "10px", padding: "2px"}}>
+    <div style={{border: "solid 2px", height: "100vh", marginRight: "10px", padding: "2px", borderRadius: "5px"}}>
       <h2>Floorplans</h2>
       <ul>
         {floorplans.map((floorplan) => (
