5f699e3560b871fe59668e62ce51ddaf4d68139c
skwangles <tree_house@Hotmail.co.nz>
2024-08-30 11:46:06 +1200

    Authoring: Deleting and removing points

--- authoring/src/PointControls.tsx
+++ authoring/src/PointControls.tsx
@@ -3,13 +3,14 @@
 // import * as THREE from 'three';
 // import RotationController from './RotationController'
 // @ts-ignore - required for Types
-import React from 'react';
+import React, { useState } from 'react';
 import {API_PREFIX} from './consts'
 import axios from 'axios';
 
 const PointControls = ({ id, point, reloadPoints }: {id: string | undefined, point: {id: string, x: number, y:number, type: number} | null, reloadPoints: any}) => {
   if (!id || !point) return (<></>)
 
+    const [connectId, setConnectId] = useState<string>("")
 
   return (
   <div style={{margin: "5px", borderRadius:"5px", border: "solid 1px", width: "fit-content", height: "fit-content", display: 'flex', flexDirection:'column'}}>
@@ -27,10 +28,35 @@ const PointControls = ({ id, point, reloadPoints }: {id: string | undefined, poi
       window.alert("Sucessfully removed!")
       reloadPoints();
     }
-    if (result.status == 409){
-     window.alert("Could not delete point! [" + result.statusText.toString() + "]"); 
+    else if (result.status == 409){
+     window.alert(`Could not delete point! [${result.statusText.toString()}]`); 
     }
-    }}>Remove Point</button>
+    else {
+      window.alert(`Unknown error: ${result.statusText}`)
+    }
+    }}>Delete Point</button>
+
+    <div> 
+      <button style={{margin: "5px"}} onClick={async ()=>{
+        const id2 = Number(connectId);
+        console.log("Connecting to:", id2)
+        if (!id2 || isNaN(id2)) return;
+
+        const result = await axios.post(`${API_PREFIX}/api/point/${id}/connect/${id2}`)
+        if (result.status == 201){
+          window.alert(`Successfully connected ID: ${id} to ID 2: ${id2}`)
+          reloadPoints();
+        }
+        else if (result.status == 409){
+          window.alert(`Could not connect points! [${result.statusText}]`)
+        }
+        else {
+          window.alert(`Unknown error: ${result.statusText}`)
+        }
+        
+      }}>Connect To</button>
+      <input type='text' id="connect-point-id" placeholder='e.g. 271' onChange={(e) => setConnectId(e.target.value)}/>
+    </div>
 </div>)
 };
 
