e8e3b56de010c79eafe4e1bca3ae9c6d52e73fbb
skwangles <tree_house@Hotmail.co.nz>
2024-08-31 17:54:54 +1200

    Production test RUNME

--- RUNME.sh
+++ RUNME.sh
@@ -5,31 +5,10 @@
 # Build the CampusVirtual control program - OR 
 g++ ./ENTRYPOINT/runCampusVirtual.cc -o runCampusVirtual -lboost_program_options
 
-# # Short testing media
-# ./runCampusVirtual true ./ short-straight.mp4 --out straight.db --media_dir /media/skwangles/KINGSTON/MEDIA/
-# ./runCampusVirtual false ./ short-turn.mp4 --out straight-turn.db --media_dir /media/skwangles/KINGSTON/MEDIA/
-
-# # View it
-# ./runCampusVirtual true ./ .  --in straight-turn.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --convertToGraph
-
-
-
 # Create Map with G block
-# ./runCampusVirtual true ./ G-block-2.mp4 --out test-json.db --media_dir /media/skwangles/KINGSTON/MEDIA/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/
-# ./runCampusVirtual true ./ G-block-smaller.mp4 --in g-block-test.db --out g.db --media_dir /media/skwangles/KINGSTON/MEDIA/
+# ./runCampusVirtual true ./ VID_20240830_134002_00_001.mp4 --out outdoors.db --media_dir /media/skwangles/KINGSTON/MEDIA/Videos --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/
+./runCampusVirtual true ./ VID_20240830_135610_00_003.mp4 --in outdoors.db --out outdoors-2.db --media_dir /media/skwangles/KINGSTON/MEDIA/Video/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/
+./runCampusVirtual true ./ VID_20240830_140935_00_004.mp4 --in outdoors-2.db --out outdoors-3.db --media_dir /media/skwangles/KINGSTON/MEDIA/Video/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/
 
-
-# # View it
-./runCampusVirtual true ./ --videos . --in test-json.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --json_dir . --convertToPg
-
-
-# # Add S block
-# ./runCampusVirtual true ./ S-Block-From-G.mp4 --in g.db --out g-s.db --media_dir /media/skwangles/KINGSTON/MEDIA/
-
-# # # Add F block & FG
-# ./runCampusVirtual true ./ FG-F-from-G.mp4,F-block.mp4 --in g-s.db --out g-s-f-fg.db --media_dir /media/skwangles/KINGSTON/MEDIA/
-
-# # # Add E block
-# ./runCampusVirtual true ./ E-block-fast.mp4 --in g-s-f-fg.db --out g-s-f-fg-e.db --media_dir /media/skwangles/KINGSTON/MEDIA/
-
-# ./runCampusVirtual true ./ . --in g-s-f-fg-e.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --convertToGraph
+# Send to Postgres
+# ./runCampusVirtual true ./ --videos . --in outdoors.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/ --convertToPg
