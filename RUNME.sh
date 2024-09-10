c06531c1b4dc686015a45a1b61a8c3ae31bdd09e
skwangles <tree_house@Hotmail.co.nz>
2024-09-11 11:40:06 +1200

    Add --picture-dir to RUNME

--- RUNME.sh
+++ RUNME.sh
@@ -5,10 +5,16 @@
 # Build the CampusVirtual control program - OR 
 g++ ./ENTRYPOINT/runCampusVirtual.cc -o runCampusVirtual -lboost_program_options
 
-# Create Map with G block
-./runCampusVirtual true ./ VID_20240830_134002_00_001.mp4 --out outdoors.db --media_dir /media/skwangles/KINGSTON/MEDIA/Video/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/
-./runCampusVirtual true ./ VID_20240830_135610_00_003.mp4 --in outdoors.db --out outdoors-2.db --media_dir /media/skwangles/KINGSTON/MEDIA/Video/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/
-./runCampusVirtual true ./ VID_20240830_140935_00_004.mp4 --in outdoors-2.db --out outdoors-3.db --media_dir /media/skwangles/KINGSTON/MEDIA/Video/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/
+# Create Map with Raw video
+./runCampusVirtual false ./ S-Block-From-G.mp4 --in g.db --out otherbuildings.db --media_dir /media/skwangles/KINGSTON/MEDIA/TestVideo/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/ --picture-dir /media/skwangles/KINGSTON/MEDIA/pictures
+./runCampusVirtual false ./ FG-F-from-G.mp4,F-block.mp4,E-block-fast.mp4 --in otherbuildings.db --out otherbuildings.db --media_dir /media/skwangles/KINGSTON/MEDIA/TestVideo/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/ --picture-dir /media/skwangles/KINGSTON/MEDIA/pictures
+./runCampusVirtual true ./ VID_20240830_134002_00_001.mp4  --in otherbuildings.db --out outdoors.db --media_dir /media/skwangles/KINGSTON/MEDIA/Video/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/ --picture-dir /media/skwangles/KINGSTON/MEDIA/pictures
+./runCampusVirtual true ./ VID_20240830_135610_00_003.mp4 --in outdoors.db --out outdoors-2.db --media_dir /media/skwangles/KINGSTON/MEDIA/Video/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/ --picture-dir /media/skwangles/KINGSTON/MEDIA/pictures
+./runCampusVirtual true ./ VID_20240830_140935_00_004.mp4 --in outdoors-2.db --out outdoors-3.db --media_dir /media/skwangles/KINGSTON/MEDIA/Video/ --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/ --picture-dir /media/skwangles/KINGSTON/MEDIA/pictures
 
 # Send to Postgres
-# ./runCampusVirtual true ./ --videos . --in outdoors.db --out . --media_dir /media/skwangles/KINGSTON/MEDIA/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/ --convertToPg
+# ./runCampusVirtual true ./ --videos . --in otherbuildings.db --out . --media_dir . --map_dir /home/skwangles/Documents/Honours/MEDIA/Maps/ --json_dir /media/skwangles/KINGSTON/MEDIA/JSON/ --convertToPg
+
+# cd ./Postgres/GraphPruner
+
+# npm run start
