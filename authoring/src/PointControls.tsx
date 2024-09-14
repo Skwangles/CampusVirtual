77cdf9605c57d0c5651d125f5c8251c8d77949cc
skwangles <tree_house@Hotmail.co.nz>
2024-09-14 17:46:59 +1200

    Add Disconnect button

--- authoring/src/PointControls.tsx
+++ authoring/src/PointControls.tsx
@@ -2,62 +2,131 @@
 // import { Canvas } from '@react-three/fiber';
 // import * as THREE from 'three';
 // import RotationController from './RotationController'
-// @ts-ignore - required for Types
-import React, { useState } from 'react';
-import {API_PREFIX} from './consts'
-import axios from 'axios';
+// @ts-ignore - required for Types/
+import React, { useState } from 'react'
+import { API_PREFIX } from './consts'
+import axios from 'axios'
 
-const PointControls = ({ id, point, reloadPoints }: {id: string | undefined, point: {id: string, x: number, y:number, type: number} | null, reloadPoints: any}) => {
-  if (!id || !point) return (<></>)
+const PointControls = ({
+  id,
+  point,
+  reloadPoints,
+}: {
+  id: string | undefined
+  point: { id: string; x: number; y: number; type: number } | null
+  reloadPoints: any
+}) => {
+  if (!id || !point) return <></>
 
-    const [connectId, setConnectId] = useState<string>("")
+  const [connectId, setConnectId] = useState<string>('')
+  const [disconnectId, setDisconnectId] = useState<string>('')
 
   return (
-  <div style={{margin: "5px", borderRadius:"5px", border: "solid 1px", width: "fit-content", height: "fit-content", display: 'flex', flexDirection:'column'}}>
-    <h2>Point Info</h2>
-    <ul>
-            <li>Point ID: {point.id}</li>
-          <li>X: {point.x * 100}%</li>
-          <li>Y: {point.y * 100}%</li>
-          <li>Type: {point.type}</li>
-    </ul>
-  <img width={400} height={200} src={`${API_PREFIX}/api/image/${id}`}/>
-  <button style={{margin: "5px"}} onClick={async () => {
-    const result = await axios.delete(`/api/point/${id}/delete`);
-    if (result.status == 200){
-      window.alert("Sucessfully removed!")
-      reloadPoints();
-    }
-    else if (result.status == 409){
-     window.alert(`Could not delete point! [${result.statusText.toString()}]`); 
-    }
-    else {
-      window.alert(`Unknown error: ${result.statusText}`)
-    }
-    }}>Delete Point</button>
+    <div
+      style={{
+        margin: '5px',
+        borderRadius: '5px',
+        border: 'solid 1px',
+        width: 'fit-content',
+        height: 'fit-content',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <h2>Point Info</h2>
+      <ul>
+        <li>Point ID: {point.id}</li>
+        <li>X: {point.x * 100}%</li>
+        <li>Y: {point.y * 100}%</li>
+        <li>Type: {point.type}</li>
+      </ul>
+      <img width={400} height={200} src={`${API_PREFIX}/api/image/${id}`} />
+      <button
+        style={{ margin: '5px' }}
+        onClick={async () => {
+          const result = await axios.delete(`/api/point/${id}/delete`)
+          if (result.status == 200) {
+            window.alert('Sucessfully removed!')
+            reloadPoints()
+          } else if (result.status == 409) {
+            window.alert(
+              `Could not delete point! [${result.statusText.toString()}]`
+            )
+          } else {
+            window.alert(`Unknown error: ${result.statusText}`)
+          }
+        }}
+      >
+        Delete Point
+      </button>
 
-    <div> 
-      <button style={{margin: "5px"}} onClick={async ()=>{
-        const id2 = Number(connectId);
-        console.log("Connecting to:", id2)
-        if (!id2 || isNaN(id2)) return;
+      <div>
+        <button
+          style={{ margin: '5px' }}
+          onClick={async () => {
+            const id2 = Number(connectId)
+            console.log('Connecting to:', id2)
+            if (!id2 || isNaN(id2)) return
 
-        const result = await axios.post(`${API_PREFIX}/api/point/${id}/connect/${id2}`)
-        if (result.status == 201){
-          window.alert(`Successfully connected ID: ${id} to ID 2: ${id2}`)
-          reloadPoints();
-        }
-        else if (result.status == 409){
-          window.alert(`Could not connect points! [${result.statusText}]`)
-        }
-        else {
-          window.alert(`Unknown error: ${result.statusText}`)
-        }
-        
-      }}>Connect To</button>
-      <input type='text' id="connect-point-id" placeholder='e.g. 271' onChange={(e) => setConnectId(e.target.value)}/>
+            const result = await axios.post(
+              `${API_PREFIX}/api/point/${id}/connect/${id2}`
+            )
+            if (result.status == 201) {
+              window.alert(`Successfully connected ID: ${id} to ID 2: ${id2}`)
+              reloadPoints()
+            } else if (result.status == 409) {
+              window.alert(`Could not connect points! [${result.statusText}]`)
+            } else {
+              window.alert(`Unknown error: ${result.statusText}`)
+            }
+          }}
+        >
+          Connect To
+        </button>
+        <input
+          type="text"
+          id="connect-point-id"
+          placeholder="e.g. 271"
+          onChange={(e) => setConnectId(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <button
+          style={{ margin: '5px' }}
+          onClick={async () => {
+            const id2 = Number(disconnectId)
+            console.log('Disconnecting from:', id2)
+            if (!id2 || isNaN(id2)) return
+
+            const result = await axios.delete(
+              `${API_PREFIX}/api/point/${id}/connect/${id2}`
+            )
+            if (result.status == 201) {
+              window.alert(
+                `Successfully disconnected ID: ${id} to ID 2: ${id2}`
+              )
+              reloadPoints()
+            } else if (result.status == 409) {
+              window.alert(
+                `Could not disconnect points - they may already be disconnected! [${result.statusText}]`
+              )
+            } else {
+              window.alert(`Unknown error: ${result.statusText}`)
+            }
+          }}
+        >
+          Disconnect From
+        </button>
+        <input
+          type="text"
+          id="disconnect-point-id"
+          placeholder="e.g. 271"
+          onChange={(e) => setDisconnectId(e.target.value)}
+        />
+      </div>
     </div>
-</div>)
-};
+  )
+}
 
-export default PointControls;
+export default PointControls
