d2f58aca7876dae9a497ec50be422c362aa5d740
skwangles <tree_house@Hotmail.co.nz>
2024-09-14 23:04:25 +1200

    Add Point filling and rotating to controls

--- authoring/src/FloorplanEditor.tsx
+++ authoring/src/FloorplanEditor.tsx
@@ -1,202 +1,286 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Stage, Layer, Image as KonvaImage, Circle, Line } from 'react-konva';
-import FileUpload from './FileUpload';
-import PointControls from './PointControls';
-import { API_PREFIX } from './consts';
-import { findPathWithDijkstra } from './pathfinding';
+import React, { useState, useEffect, useRef, useReducer } from 'react'
+import axios from 'axios'
+import { Stage, Layer, Image as KonvaImage, Circle, Line } from 'react-konva'
+import FileUpload from './FileUpload'
+import PointControls from './PointControls'
+import { API_PREFIX } from './consts'
+import { findPathWithDijkstra } from './pathfinding'
 
 interface Node {
-  id: string;
-  x: number;
-  y: number;
-  type: number;
+  id: string
+  x: number
+  y: number
+  type: number
 }
 
 interface Edge {
-  id0: string;
-  id1: string;
+  id0: string
+  id1: string
 }
 
 interface FloorplanEditorProps {
-  floorplan: string;
+  floorplan: string
 }
 
 const FloorplanEditor: React.FC<FloorplanEditorProps> = ({ floorplan }) => {
-  const [nodes, setNodes] = useState<Node[]>([]);
-  const [edges, setEdges] = useState<Edge[]>([]);
-  const [hasImage, setHasImage] = useState<boolean>(false);
-  const [image, setImage] = useState<HTMLImageElement | null>(null);
-  const [stageWidth, setStageWidth] = useState<number>(800);
-  const [stageHeight, setStageHeight] = useState<number>(800);
-  const [selectedPoints, setSelectedPoints] = useState<Set<string>>(new Set());
-  const [lastSelectedPoint, setLastSelectedPoint] = useState<Node | null>(null);
+  const [nodes, setNodes] = useState<Node[]>([])
+  const [edges, setEdges] = useState<Edge[]>([])
+  const [hasImage, setHasImage] = useState<boolean>(false)
+  const [image, setImage] = useState<HTMLImageElement | null>(null)
+  const [stageWidth, setStageWidth] = useState<number>(800)
+  const [stageHeight, setStageHeight] = useState<number>(800)
+  const [selectedPoints, setSelectedPoints] = useState<Set<string>>(new Set())
+  const [lastSelectedPoint, setLastSelectedPoint] = useState<Node | null>(null)
+  const [, forceUpdate] = useReducer((x) => x + 1, 0)
 
-  const [dragStartPos, setDragStartPos] = useState<{x: number, y: number}| null>(null)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const layer = useRef<any>(null)
 
+  const [dragStartPos, setDragStartPos] = useState<{
+    x: number
+    y: number
+  } | null>(null)
 
   const fetchFloorplanData = async () => {
     try {
       const response = await axios.get<{
-        has_image: boolean;
-        image: string;
-        edges: Array<{ keyframe_id0: number; keyframe_id1: number }>;
-        nodes: Array<{ x: number; y: number; keyframe_id: number; type: number }>;
-      }>(`${API_PREFIX}/api/floorplans/${floorplan}`);
-      console.log(response.data);
-      const img = new Image();
-      img.src = `${API_PREFIX}/api/floorplans/${floorplan}/image`;
-      setHasImage(Boolean(response.data.has_image));
+        has_image: boolean
+        image: string
+        edges: Array<{ keyframe_id0: number; keyframe_id1: number }>
+        nodes: Array<{
+          x: number
+          y: number
+          keyframe_id: number
+          type: number
+        }>
+      }>(`${API_PREFIX}/api/floorplans/${floorplan}`)
+      console.log(response.data)
+      const img = new Image()
+      img.src = `${API_PREFIX}/api/floorplans/${floorplan}/image`
+      setHasImage(Boolean(response.data.has_image))
       img.onload = () => {
-        setImage(img);
-        setStageWidth(img.width);
-        setStageHeight(img.height);
-      };
+        setImage(img)
+        setStageWidth(img.width)
+        setStageHeight(img.height)
+      }
       setEdges(
         response.data.edges.map((edge) => ({
           id0: String(edge.keyframe_id0),
           id1: String(edge.keyframe_id1),
         }))
-      );
+      )
       setNodes(
         response.data.nodes.map((node) => ({
           id: String(node.keyframe_id),
           ...node,
         }))
-      );
+      )
     } catch (error) {
-      console.error('Error fetching floorplan data:', error);
+      console.error('Error fetching floorplan data:', error)
     }
-  };
+  }
 
   useEffect(() => {
     setSelectedPoints(new Set())
-    fetchFloorplanData();
-  }, [floorplan]);
+    fetchFloorplanData()
+  }, [floorplan])
 
   const handleDragStart = (e: any) => {
-    const { x, y } = e.target.position();
-    setDragStartPos({ x, y });
-  };
-
+    const { x, y } = e.target.position()
+    setDragStartPos({ x, y })
+  }
 
   const handleDragMove = (e: any) => {
     console.log(dragStartPos)
-    if (!dragStartPos) return;
+    if (!dragStartPos) return
 
+    const { x: x, y: y } = e.target.position()
+    const dx = x - dragStartPos.x
+    const dy = y - dragStartPos.y
 
-    const { x: x, y: y } = e.target.position();
-    const dx = x - dragStartPos.x;
-    const dy = y - dragStartPos.y;
-
-    const width = e.target.getStage().width();
-    const height = e.target.getStage().height();
+    const width = e.target.getStage().width()
+    const height = e.target.getStage().height()
 
     setNodes((prevNodes) =>
       prevNodes.map((node) => {
         if (selectedPoints.has(node.id) && node.id != e.target.name()) {
-          const newX = clamp(node.x + (dx / width), 0, 1);
-          const newY = clamp(node.y + (dy / height), 0, 1);
-          return { ...node, x: newX, y: newY };
+          const newX = clamp(node.x + dx / width, 0, 1)
+          const newY = clamp(node.y + dy / height, 0, 1)
+          return { ...node, x: newX, y: newY }
+        } else if (node.id == e.target.name()) {
+          return { ...node, x: x / width, y: y / height }
         }
-        else if (node.id == e.target.name()){
-          return {...node, x: x / width, y: y / height}
-        }
-        return node;
+        return node
       })
-    );
+    )
 
-    setDragStartPos({x, y})
-  };
+    setDragStartPos({ x, y })
+  }
 
   const handleMouseDown = (e: any) => {
-    const id = e.target.name();
-    const foundNode = nodes.find((node) => String(node.id) === String(id));
-    const isCtrlPressed = e.evt.ctrlKey;
-    const isShiftPressed = e.evt.shiftKey;
+    const id = e.target.name()
+    const foundNode = nodes.find((node) => String(node.id) === String(id))
+    const isCtrlPressed = e.evt.ctrlKey
+    const isShiftPressed = e.evt.shiftKey
 
     if (isCtrlPressed) {
       setSelectedPoints((prev) => {
-        const newSelected = prev;
+        const newSelected = prev
         if (newSelected.has(id)) {
-          newSelected.delete(id);
+          newSelected.delete(id)
         } else {
-          newSelected.add(id);
+          newSelected.add(id)
         }
-        return newSelected;
-      });
+        return newSelected
+      })
     } else if (isShiftPressed && lastSelectedPoint) {
-      const method = 1; // Change to vary dijkstra method - only 1 currently implemented 
+      const method = 1 // Change to vary dijkstra method - only 1 currently implemented
 
-      const selectedIds = findPathWithDijkstra(nodes, edges, lastSelectedPoint.id, id, method, true);
-      setSelectedPoints(prev => {
-        for (const id of selectedIds){
+      const selectedIds = findPathWithDijkstra(
+        nodes,
+        edges,
+        lastSelectedPoint.id,
+        id,
+        method,
+        true
+      )
+      setSelectedPoints((prev) => {
+        for (const id of selectedIds) {
           prev.add(id)
         }
         return prev
-      });
+      })
     } else {
-      if (selectedPoints.size <= 1){
-      setSelectedPoints(new Set([id]));
-      }
-      else{
-        setSelectedPoints(prev => prev.add(id))
+      if (selectedPoints.size <= 1) {
+        setSelectedPoints(new Set([id]))
+      } else {
+        setSelectedPoints((prev) => prev.add(id))
       }
     }
-  
-    setLastSelectedPoint(foundNode || null);
-  };
-  
+
+    setLastSelectedPoint(foundNode || null)
+  }
 
   const handleMouseUp = (e: any) => {
-    const x = e.target.x() / e.target.getStage().width();
-    const y = e.target.y() / e.target.getStage().height();
+    const x = e.target.x() / e.target.getStage().width()
+    const y = e.target.y() / e.target.getStage().height()
 
-    console.log("Finish pos:", x, y) 
+    console.log('Finish pos:', x, y)
     setDragStartPos(null)
-    for (const node of nodes.filter(node => selectedPoints.has(node.id))){
-        axios.post(`${API_PREFIX}/api/floorplans/${floorplan}/update`, { id: node.id, x: node.x, y: node.y });
+    for (const node of nodes.filter((node) => selectedPoints.has(node.id))) {
+      axios.post(`${API_PREFIX}/api/floorplans/${floorplan}/update`, {
+        id: node.id,
+        x: node.x,
+        y: node.y,
+      })
     }
-  };
+  }
 
   const padCordAwayFromEdge = (val: number) => {
-    const padding = 0.01;
+    const padding = 0.01
     return clamp(val, 0 + padding, 1 - padding)
-  };
+  }
 
   const clamp = (value: number, min: number, max: number) => {
-    return Math.max(min, Math.min(max, value));
-  };
+    return Math.max(min, Math.min(max, value))
+  }
 
   const getNodeCoords = (id: string) => {
-    const node = nodes.find((node) => node.id === id);
-    if (!node) throw Error("Couldn't find node: " + id);
+    const node = nodes.find((node) => node.id === id)
+    if (!node) throw Error("Couldn't find node: " + id)
     return {
       x: padCordAwayFromEdge(node.x) * stageWidth,
       y: padCordAwayFromEdge(node.y) * stageHeight,
-    };
-  };
+    }
+  }
+
+  const flipPointsVertically = (onlySelected = false) => {
+    setNodes((prevNodes) => {
+      prevNodes.map((node) => {
+        if (onlySelected && !selectedPoints.has(node.id)) return node
+        node.y = padCordAwayFromEdge(1 - node.y)
+        return node
+      })
+
+      return prevNodes
+    })
+    forceUpdate()
+  }
+
+  const flipPointsHorizontally = (onlySelected = false) => {
+    setNodes((prevNodes) => {
+      prevNodes.map((node) => {
+        if (onlySelected && !selectedPoints.has(node.id)) return node
+        node.x = padCordAwayFromEdge(1 - node.x)
+        return node
+      })
+
+      return prevNodes
+    })
+    forceUpdate()
+  }
+  const rotatePoints = (onlySelected = false) => {
+    setNodes((prevNodes) => {
+      prevNodes.map((node) => {
+        if (onlySelected && !selectedPoints.has(node.id)) return node
+        const x = node.x
+        const y = node.y
+        node.x = padCordAwayFromEdge(y)
+        node.y = padCordAwayFromEdge(1 - x)
+        return node
+      })
+
+      return prevNodes
+    })
+    forceUpdate()
+  }
 
   return (
     <>
       <div>
         <FileUpload currentFloor={floorplan} isImageSelected={hasImage} />
+        <div style={{ display: 'flex', flexBasis: '10%', flexWrap: 'wrap' }}>
+          <button onClick={() => rotatePoints()}>
+            Rotate All Points 90 Degrees
+          </button>
+          <button onClick={() => flipPointsVertically()}>
+            Flip All Points Vertically
+          </button>
+          <button onClick={() => flipPointsHorizontally()}>
+            Flip All Points Horizontally
+          </button>
+          <button onClick={() => rotatePoints(true)}>
+            Rotate Selected (Green) Points 90 Degrees
+          </button>
+          <button onClick={() => flipPointsVertically(true)}>
+            Flip Selected Points Vertically
+          </button>
+          <button onClick={() => flipPointsHorizontally(true)}>
+            Flip Selected Points Horizontally
+          </button>
+        </div>
         <div style={{ border: 'solid 1px', borderRadius: '5px' }}>
           {hasImage && (
             <Stage
               width={stageWidth}
               height={stageHeight}
+              ref={layer}
               // draggable
               // onDragMove={handleDragMove}
               // onDragEnd={handleDragEnd}
             >
               <Layer>
-                {image && (
+                {image && nodes && (
                   <>
-                    <KonvaImage image={image} width={stageWidth} height={stageHeight} onMouseDown={() => setSelectedPoints(new Set())} />
+                    <KonvaImage
+                      image={image}
+                      width={stageWidth}
+                      height={stageHeight}
+                      onMouseDown={() => setSelectedPoints(new Set())}
+                    />
                     {edges.map((edge, index) => {
-                      const { x: x1, y: y1 } = getNodeCoords(edge.id0);
-                      const { x: x2, y: y2 } = getNodeCoords(edge.id1);
+                      const { x: x1, y: y1 } = getNodeCoords(edge.id0)
+                      const { x: x2, y: y2 } = getNodeCoords(edge.id1)
                       return (
                         <Line
                           key={index}
@@ -206,26 +290,33 @@ const FloorplanEditor: React.FC<FloorplanEditorProps> = ({ floorplan }) => {
                           lineCap="round"
                           lineJoin="round"
                         />
-                      );
+                      )
                     })}
-                    {nodes.map((node) => (
-                      <Circle
-                        key={node.id}
-                        x={padCordAwayFromEdge(node.x) * stageWidth}
-                        y={padCordAwayFromEdge(node.y) * stageHeight}
-                        radius={5}
-                        fill={selectedPoints.has(node.id) ? 'green' : node.type < 50 ? 'red' : 'blue'}
-                        name={node.id}
-                        draggable
-                        onMouseDown={handleMouseDown}
-                        onTouchStart={handleMouseDown}
-                        onMouseUp={handleMouseUp}
-                        onTouchEnd={handleMouseUp}
-                        onDragStart={handleDragStart}
-                        onDragMove={handleDragMove}
-                        // onDragEnd={handleDragEnd}
-                      />
-                    ))}
+                    {nodes &&
+                      nodes.map((node) => (
+                        <Circle
+                          key={node.id}
+                          x={padCordAwayFromEdge(node.x) * stageWidth}
+                          y={padCordAwayFromEdge(node.y) * stageHeight}
+                          radius={5}
+                          fill={
+                            selectedPoints.has(node.id)
+                              ? 'green'
+                              : node.type < 50
+                              ? 'red'
+                              : 'blue'
+                          }
+                          name={node.id}
+                          draggable
+                          onMouseDown={handleMouseDown}
+                          onTouchStart={handleMouseDown}
+                          onMouseUp={handleMouseUp}
+                          onTouchEnd={handleMouseUp}
+                          onDragStart={handleDragStart}
+                          onDragMove={handleDragMove}
+                          // onDragEnd={handleDragEnd}
+                        />
+                      ))}
                   </>
                 )}
               </Layer>
@@ -238,13 +329,13 @@ const FloorplanEditor: React.FC<FloorplanEditorProps> = ({ floorplan }) => {
           id={lastSelectedPoint?.id}
           point={lastSelectedPoint}
           reloadPoints={() => {
-            console.log('Reloading points');
-            fetchFloorplanData();
+            console.log('Reloading points')
+            fetchFloorplanData()
           }}
         />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default FloorplanEditor;
+export default FloorplanEditor
