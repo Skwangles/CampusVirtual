7521a35fe855cf49dccdc21ddbecf276a326d1af
skwangles <tree_house@Hotmail.co.nz>
2024-09-18 21:21:57 +1200

    rename file to Cd into

--- SETUP_PG.sh
+++ SETUP_PG.sh
@@ -1,7 +1,8 @@
-cd Postgres
+cd Config
 
 sudo apt install libpqxx-dev
 
 docker-compose --env-file ../.env up -d
 
 cd ..
+echo "Note: You will have to change the credentials in all db.ts files in Apps/ to match - Goodluck!"
