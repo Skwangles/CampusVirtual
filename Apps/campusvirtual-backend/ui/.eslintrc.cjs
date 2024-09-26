c91eb3c3de7b18e1ccabd30b686626fd6a0df54b
skwangles <tree_house@Hotmail.co.nz>
2024-09-26 21:00:14 +1200

    update eslintrc

--- Apps/campusvirtual-backend/ui/.eslintrc.cjs
+++ Apps/campusvirtual-backend/ui/.eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'eslint/no-explicit-any': "warn",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
