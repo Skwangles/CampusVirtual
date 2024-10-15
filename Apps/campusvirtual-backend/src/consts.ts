ce76d511c18dbdd002aa2e44a93d8639d411085c
skwangles <tree_house@Hotmail.co.nz>
2024-10-15 20:39:03 +1300

    update pictures dir

--- Apps/campusvirtual-backend/src/consts.ts
+++ Apps/campusvirtual-backend/src/consts.ts
@@ -1,6 +1,6 @@
 export const BORDERING_FLOOR_POINT_DEFAULT_TYPE = 50;
 export const FLOOR_POINT_DEFAULT_TYPE = 0;
-export const KEYFRAME_IMG_DIR = '/media/skwangles/KINGSTON/MEDIA/pictures/'
+export const KEYFRAME_IMG_DIR = '/media/skwangles/KINGSTON/MEDIA/eds-pictures/'
 export const SEND_TEST_IMAGE = false
 export const COORDS_TO_METRES = 10;
 export const FLOORPLAN_IMAGE_DIR = "/home/skwangles/Documents/Honours/CampusVirtual/floorplans/"
